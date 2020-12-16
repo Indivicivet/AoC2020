1f445b4ae24541bd09f39c37b07c0929145bc10e
Indi <indi.pritchard@vivid-q.com>
2020-12-16 16:26:25 +0000

    update all qns up to q14 to use a separate input file

--- q13.py
+++ q13.py
@@ -1,5 +1,5 @@
-INPUT = """1000511
-29,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,409,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,17,13,19,x,x,x,23,x,x,x,x,x,x,x,353,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41""".splitlines()
+with open("input/q13.txt") as f:
+    INPUT = f.read().splitlines()
 
 print(
     (lambda min_time, buses: 
