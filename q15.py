137187c8e02b2fd9c05fc9a051a8e8b3e818cd5c
Indi <indi.pritchard@vivid-q.com>
2020-12-16 12:39:01 +0000

    add question 15

---
+++ q15.py
@@ -0,0 +1,24 @@
+INPUT = [16, 11, 15, 0, 1, 7]
+
+print(
+    (lambda turns, said_times:
+        max(enumerate(
+            (
+                turns.__iadd__([len(turns) - said_times.get(turns[-1], len(turns))]),
+                said_times.update({turns[-2]: len(turns) - 1}),
+            )
+            for _ in range(len(turns), 2020)
+        ))[1][0][-1]
+    )(INPUT, {v: i + 1 for i, v in enumerate(INPUT)})
+)
+print(
+    (lambda turns, said_times:
+        max(enumerate(
+            (
+                turns.__iadd__([len(turns) - said_times.get(turns[-1], len(turns))]),
+                said_times.update({turns[-2]: len(turns) - 1}),
+            )
+            for _ in range(len(turns), 30000000)  # otherwise identical
+        ))[1][0][-1]
+    )(INPUT, {v: i + 1 for i, v in enumerate(INPUT)})
+)
