854dfde01cd2c36766407e2550f6118e68c86124
Indi <indivicivet@gmail.com>
2020-12-18 06:03:20 +0000

    add question 18 :))

---
+++ q18.py
@@ -0,0 +1,19 @@
+with open("input/q18.txt") as f:
+    INPUT = f.read().splitlines()
+
+print(
+    (lambda cls: sum(eval("".join(
+        t if t in "()+" else "-" if t == "*" else f"{cls.__name__}({t})"
+        for t in line.replace("(", "( ").replace(")", " )").split(" ")
+    )) for line in INPUT))(
+        cls := type("cls", (int,), {"__add__": lambda s, o: cls(int(s) + o), "__sub__": lambda s, o: cls(int(s) * o)})
+    )
+)
+print(
+    (lambda cls: sum(eval("".join(
+        {"(": "(", ")": ")", "+": "*", "*": "+"}.get(t, f"{cls.__name__}({t})")
+        for t in line.replace("(", "( ").replace(")", " )").split(" ")
+    )) for line in INPUT))(
+        cls := type("cls", (int,), {"__add__": lambda s, o: cls(int(s) * o), "__mul__": lambda s, o: cls(int(s) + o)})
+    )
+)
